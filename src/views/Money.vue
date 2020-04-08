3bb9edb55cf1eeb9d30c741795a140b909f9134c
csy <13540039093@163.com>
2020-04-08 16:23:27 +0800

    money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,17 +1,45 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+    </div>
     <Nav/>
   </div>
 </template>
 
 <script lang="ts">
 
-  export default {
-
-  };
+  export default {};
 </script>
 
 <style lang="scss" scoped>
+  .nav-wrapper {
+    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
 
+  .content {
+    border: 1px solid blue;
+    flex-grow: 1;
+    overflow: auto;
+  }
 </style>
