46c2ed25133725ee20e1dadc22215a3dd40bbf83
csy <13540039093@163.com>
2020-04-09 16:15:27 +0800

    nav 布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-    <router-link to="/money">记账</router-link>
-    |
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="statistics">统计</router-link>
-  </div>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
+      <icon name="money"/>
+      记账
+    </router-link>
+    <router-link to="/labels" class="item" active-class="selected">
+      <icon name="label"/>
+      标签
+    </router-link>
+    <router-link to="statistics" class="item" active-class="selected">
+      <icon name="statistics"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -15,5 +22,25 @@
 </script>
 
 <style lang="scss" scoped>
+  nav {
+    display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
 
+    > .item {
+      width: 33.3333333%;
+      display: flex;
+      justify-content: center;  //指定方向上居中
+      align-items: center;  //指定方向的垂直方向居中
+      font-size: 12px;
+      flex-direction: column;
+      padding: 3px 0;
+    }
+      .icon{
+        width:32px;
+        height:32px;
+      }
+    >.selected{
+      color: red;
+    }
+  }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
+const path =require ('path') //引入一个path模块，是nodejs的一个模块，可以用来做path
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config =>{
+    const dir = path.resolve(__dirname, 'src/assets/icons') //确定icon所在的目录，__dirname当前目录的意思
+
+    config.module
+      .rule('svg-sprite')  //规则名称
+      .test(/\.svg$/)  //匹配此正则的文件（以.svg结尾的文件）
+      .include.add(dir).end() // 包含 icons 目录
+      //匹配loader,extract:false不要解析出文件
+      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract:false}).end()
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
+    //匹配插件
+    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
+    config.module.rule('svg').exclude.add(dir)// 其他 svg loader 排除 icons 目录
+  }
 }
